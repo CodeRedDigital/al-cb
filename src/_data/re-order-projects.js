2ffde6233d044a4568af473f80a1a81be2307cff
Dave Letorey <dave@code-red.uk>
2021-07-24 12:56:50 +0100

    switched back to original helper

--- src/_data/re-order-projects.js
+++ src/_data/re-order-projects.js
@@ -3,7 +3,7 @@ const helpers = require('./helpers')
 module.exports = async () => {
   try {
     // Grabs either the fresh remote data or cached data (will always be fresh live)
-    const results = await helpers.getPrismicProjects("projects_slice");
+    const results = await helpers.getPrismicResults("projects_slice");
 
     return results;
   } catch (ex) {
