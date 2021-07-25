89dbc9f7004e5eeaae509396297d1c100a6e6b0d
Dave Letorey <dave@code-red.uk>
2021-07-25 13:56:11 +0100

    renamed the js file to have no dashes and updated where the data comes from

--- src/_data/reorderProjects.js
+++ src/_data/reorderProjects.js
@@ -3,13 +3,13 @@ const helpers = require('./helpers')
 module.exports = async () => {
   try {
     // Grabs either the fresh remote data or cached data (will always be fresh live)
-    const results = await helpers.getPrismicResults("projects_slice");
-
-    return results;
+    const slice = await helpers.getPrismicResults("projects_slice");
+    const projects = slice.results[0].data.body;
+    return projects;
   } catch (ex) {
     console.log(ex);
 
     // If failed, return back an empty array
-    return [];
+    return [1,2,3,4];
   }
 };
